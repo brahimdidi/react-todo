867e35f7b5b33706baea6e554f765f0140fb888e
brahim <brahimgb1team@gmail.com>
2022-06-09 17:56:19 +0000

    use svg icons for add and remove btn

--- src/functionComp/InputTodo.js
+++ src/functionComp/InputTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-
+import { FaPlusCircle } from "react-icons/fa"
 const InputTodo = (props) => {
   const [inputText, setInputText] = useState({
     title: "",
@@ -33,7 +33,7 @@ const InputTodo = (props) => {
         name="title"
         onChange={onChange}
       />
-      <button className="input-submit">Submit</button>
+      <button className="input-submit"><FaPlusCircle style={{ color: "darkcyan", fontSize: "20px", marginTop: "2px" }} /></button>
     </form>
     )
 
